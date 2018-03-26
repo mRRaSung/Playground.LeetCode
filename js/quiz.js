f250086f2b3c39a335ad5e3ccc12b41ced88ca3e
mRRa <spiriturl@gmail.com>
2018-03-26 22:13:18 +0800

    #292 / #283

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,4 @@
+//#344
 var reverseString = function(s){
 
 	var result = "";
@@ -7,5 +8,44 @@ var reverseString = function(s){
 		result += ary[i];
 	}
 
+	alert(result);
 	return result;
+}
+
+//#292
+var canWin = function(n){
+
+	var result = false;
+
+	if(n < 4)
+	{
+		result = true;
+	}
+	else
+	{
+		result = n % 4 != 0;
+	}
+
+	alert(result);
+	return result;
+}
+
+//#283
+var moveZeros = function(nums)
+{
+	var index = 0;
+
+	for (var i = 0; i < nums.length; i++) 
+	{
+		if(nums[i] !== 0)
+		{
+			nums[index++] = nums[i];
+		}
+	}
+
+	for (index; index < nums.length; index++) {
+		nums[index] = 0;
+	}
+
+	alert(nums);
 }
